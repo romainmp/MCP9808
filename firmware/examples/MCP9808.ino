9d37e3c77072fa2b6bd7ca53801bd6967acd5002
Romain MP <romain.mp@gmail.com>
2014-08-16 15:57:06 +0200

    Updated example

--- firmware/examples/MCP9808.ino
+++ firmware/examples/MCP9808.ino
@@ -11,22 +11,23 @@ MCP9808 mcp = MCP9808();
 void setup()
 {
 	Serial.begin(9600);
-
+    
+    delay(5000);
+    
 	Serial.println("MCP9808 test");
 
 	while(! mcp.begin()){
 	    Serial.println("MCP9808 not found");
-	    delay(1000);
+	    delay(500);
 	}
+	
+	mcp.setResolution(MCP9808_SLOWEST);
 
 	Serial.println("MCP9808 OK");
 }
 
 void loop()
 {
-	Serial.println("===================");
-	Serial.print("Temp:"); Serial.println(mcp.getTemperature());
-	Serial.println();
-
-	delay(1000);
+	Serial.print("Temp:"); Serial.println(mcp.getTemperature(), 4);
+	delay(250);
 }
